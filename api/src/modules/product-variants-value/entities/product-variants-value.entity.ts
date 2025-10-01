717433dad26fc53a471904a84c5665bff902e21f
long-lv <luongttxdks@gmail.com>
2025-10-01 14:12:32 +0700

    fix conflig

--- api/src/modules/product-variants-value/entities/product-variants-value.entity.ts
+++ api/src/modules/product-variants-value/entities/product-variants-value.entity.ts
@@ -1,6 +1,6 @@
 import { BaseEntity } from 'src/core/database/base.entity';
-import { ProductAttributeValue } from 'src/product-attribute-values/entities/product-attribute-value.entity';
-import { ProductVariant } from 'src/product-variants/entities/product-variant.entity';
+import { ProductAttributeValue } from 'src/modules/product-attribute-values/entities/product-attribute-value.entity';
+import { ProductVariant } from 'src/modules/product-variants/entities/product-variant.entity';
 import { Entity, JoinColumn, ManyToOne } from 'typeorm';
 
 @Entity('productVariantsValues')
